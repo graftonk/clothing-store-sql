fb1cbb32181f248607dfc6a48ff57c717cd509ad
graftonk <165220432+graftonk@users.noreply.github.com>
2025-04-10 01:10:36 -0400

    Update SQLQuery1.sql

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,9 +1,10 @@
 -- Create the ClothingStore Database
 Use master;
 Go
-Drop database ClothingStore;
+drop database ClothingStore;
 Go
-Create database ClothingStore;
+create database ClothingStore;
+go
 
 /*
    Create the PERSON table
@@ -18,7 +19,7 @@ create table Person (
 	State char(2) Not Null default 'MI',
 	ZipCode char(10) Not Null,
 	Email varchar(25) Not Null default 'No Email',
-	Phone char(10) Not Null default 'No Phone',
+	Phone char(12) Not Null default 'No Phone',
 	PersonType char(1) Not Null default 'C',
 -- table constraints
 	constraint person_personid_pk primary key(PersonID),
@@ -72,3 +73,28 @@ create table Customer (
 -- SUPPLIER TABLE
 
 -- PAYMENT TABLE
+
+-- INSERT INTO STATEMENTS
+/*
+   Insert sample data into the PERSON table
+   Written by Kaylee Grafton
+*/
+insert into Person (FirstName, LastName, Address, City, State, ZipCode, Email, Phone, PersonType)
+values
+('Teisha', 'Alexander', '123 Maplewood St', 'Roseville', 'MI', '48066', 'talexander@gmail.com', '586-123-4567', 'E'),
+('Freddie', 'Clark', '456 Cherry Blossom Ave', 'Warren', 'MI', '48088', 'fredclark@yahoo.com', '586-234-5678', 'E'),
+('Matthew', 'Knight', '123 Meadowbrook Blvd', 'Macomb', 'MI', '48044', 'matthew.knight@aol.com', '586-345-6789', 'E'),
+('Anna', 'Nicholson', '456 Autumn Ridge Rd', 'Macomb', 'MI', '48042', 'anicholson@gmail.com', '586-456-7890', 'E'),
+('Sidney', 'Price', '987 Riverbend Rd', 'Clinton Twp', 'MI', '48038', 'sidneyprice@gmail.com', '586-567-8901', 'E'),
+('Tanner', 'Cooke', '123 Birchwood Dr', 'Sterling Heights', 'MI', '48312', 'tannercooke@aol.com', '586-678-9012', 'E'),
+('Alexis', 'Kramer', '456 Copperfield Ln', 'Sterling Heights', 'MI', '48313', 'alexiskramer@yahoo.com', '586-789-0123', 'E'),
+('Aiden', 'Browning', '345 Sycamore St', 'Warren', 'MI', '48089', 'abrowning@gmail.com', '586-890-1234', 'B'),
+('Devon', 'Miller', '789 Oakridge Dr', 'Roseville', 'MI', '48066', 'devonmiller@outlook.com', '586-901-2345', 'B'),
+('Jackson', 'Carter', '567 Crestwood Dr', 'Clinton Twp', 'MI', '48038', 'jacksoncarter@hotmail.com', '586-012-3456', 'B'),
+('Leah', 'Gibson', '654 Sunrise Blvd', 'Sterling Heights', 'MI', '48313', 'leahgibson@yahoo.com', '586-213-4567', 'C'),
+('Jamie', 'Kelley', '654 Valley View St', 'Macomb', 'MI', '48044', 'jkelly@hotmail.com', '586-324-5678', 'C'),
+('Charles', 'Saunders', '890 Aspen Grove Dr', 'Roseville', 'MI', '48066', 'csaunders@gmail.com', '586-435-6789', 'C'),
+('Jessie', 'Norris', '234 Willow Creek Rd', 'Roseville', 'MI', '48066', 'bitethebullet@aol.com', '586-546-7890', 'C'),
+('Skylar', 'Hughes', '567 Poplar Way', 'Warren', 'MI', '48088', 'skyhughes@gmail.com', '586-657-8901', 'C'),
+('Keira', 'Edwards', '789 Maple Crest Ct', 'Sterling Heights', 'MI', '48310', 'pieceofcake@yahoo.com', '586-768-9012', 'C'),
+('Sophia', 'Webb', '890 Greenfield Ln', 'Clinton Twp', 'MI', '48035', 'sophiawebb@gmail.com', '586-879-0123', 'C');
